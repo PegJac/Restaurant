6a6914138c0c9e700924e4b1443767fd083d7976
marv-ink <marvin96k@gmail.com>
2021-08-24 17:24:58 +0200

    added page not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from "react";
-import LandingPageComponent from "./components/LandingPageComponent";
-import MenuComponent from "./components/Menu/MenuComponent";
-import BookingsComponent from "./components/Bookings/BookingsComponent";
+import React from 'react';
+import LandingPageComponent from './components/LandingPageComponent';
+import MenuComponent from './components/Menu/MenuComponent';
+import BookingsComponent from './components/Bookings/BookingsComponent';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Switch>
-          <Route path="/bookings">
-            <BookingsComponent />
-          </Route>
-          <Route path="/">
+          <Route exact path='/'>
             <LandingPageComponent />
             <MenuComponent />
+          </Route>
+          <Route path='/bookings'>
+            <BookingsComponent />
+          </Route>
+          <Route path='*'>
+            <PageNotFound></PageNotFound>
           </Route>
         </Switch>
       </Router>
