0f2df5187c5e7a9b2df7d0eee6f17f6a5735b726
PegJac <peggy.jacobson@outlook.com>
2021-08-24 11:06:29 +0200

    resolved conflicts

--- src/components/LandingPageComponent.tsx
+++ src/components/LandingPageComponent.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function LandingPageComponent() {
     return (
         <div id="landingPageContainer">
-            <h1>late night brunch</h1>
+            <h1>late night brunch.</h1>
             <Link to="/booking">BOOKING</Link>
             <Link to="/contact">CONTACT</Link>
         </div>
