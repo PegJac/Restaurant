e1a9a7584a4ab39812cb964d53d001bdf8239567
marv-ink <marvin96k@gmail.com>
2021-08-30 18:24:32 +0200

    Mob programing accepting gdpr conditions

--- src/components/Bookings/ChildComponents/GuestInfoComponent.tsx
+++ src/components/Bookings/ChildComponents/GuestInfoComponent.tsx
@@ -1,26 +1,35 @@
-import { ChangeEvent } from "react"
-import react from "react"
-import { IGuestInfoComponent } from "../../models/IGuestInfoComponent"
+import { ChangeEvent } from 'react';
+import react from 'react';
+import { IGuestInfoComponent } from '../../models/IGuestInfoComponent';
 
 export function GuestInfoComponent(props: IGuestInfoComponent) {
+  function updateUserInfo(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value, checked } = e.target;
 
-    function updateUserInfo(e: React.ChangeEvent<HTMLInputElement>) {
-        const { name, value } = e.target
-        props.updateInformation(name, value)
-        console.log(name + value)
+    if (name === 'acceptedGDPR') {
+      props.updateInformation(name, checked.toString());
+    } else {
+      props.updateInformation(name, value);
     }
+  }
 
-    return (
-        <div>
-            <input onChange={(e) => updateUserInfo(e)} autoComplete="no" type="text" placeholder="First name" name="firstName" />
-            <input onChange={(e) => updateUserInfo(e)} autoComplete="no" type="text" placeholder="Last name" name="lastName" />
-            <input onChange={(e) => updateUserInfo(e)} autoComplete="no" type="email" placeholder="Email" name="email" />
-            <input onChange={(e) => updateUserInfo(e)} autoComplete="no" type="number" placeholder="Phone" name="number" />
+  return (
+    <div>
+      <input onChange={(e) => updateUserInfo(e)} autoComplete='no' type='text' placeholder='First name' name='firstName' />
+      <input onChange={(e) => updateUserInfo(e)} autoComplete='no' type='text' placeholder='Last name' name='lastName' />
+      <input onChange={(e) => updateUserInfo(e)} autoComplete='no' type='email' placeholder='Email' name='email' />
+      <input onChange={(e) => updateUserInfo(e)} autoComplete='no' type='number' placeholder='Phone' name='number' />
 
-            <p>det här är en länk till gdpr</p>
-            <input type="checkbox" />
+      <p>det här är en länk till gdpr</p>
+      <input
+        type='checkbox'
+        name='acceptedGDPR'
+        onChange={(e) => {
+          updateUserInfo(e);
+        }}
+      />
 
-            <button type="button">Book</button>
-        </div>
-    )
+      <button type='button'>Book</button>
+    </div>
+  );
 }
