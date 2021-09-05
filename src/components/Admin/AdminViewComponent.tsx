3811cb298db5b6b07ab861bd82032438ea7c1c37
marv-ink <marvin96k@gmail.com>
2021-09-06 01:40:30 +0200

    admin is able to view all the reservations

--- src/components/Admin/AdminViewComponent.tsx
+++ src/components/Admin/AdminViewComponent.tsx
@@ -1,7 +1,32 @@
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { db } from "../../firebase";
+
 export const AdminViewComponent = () => {
+  // Getting the collection and sorting it after date
+  const bookinsRef = db.collection("bookings").orderBy("date");
+
+  //Fetching data by using a firebase hook.
+  const [snapshot, loading, error] = useCollectionData(bookinsRef);
+  console.log(snapshot);
+
+  // the rendered HTML
+  let div = snapshot?.map((booking, index) => {
+    return (
+      <div key={index}>
+        <h3>{`${booking.firstName} ${booking.lastName}`}</h3>
+        <p>{booking.date}</p>
+        <p>{booking.sitting}</p>
+        <p>{booking.numberOfGuests}</p>
+        <p>{booking.numberOfTables}</p>
+        <button>Change booking</button>
+        <button>Remove booking</button>
+      </div>
+    );
+  });
   return (
     <div>
       <h1>AdminView's</h1>
+      <div>{snapshot ? div : <h2>Looking for bookings</h2>}</div>
     </div>
   );
 };
